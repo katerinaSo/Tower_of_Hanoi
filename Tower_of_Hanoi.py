6ef39d44a9bb32ca502723540935640ed3c9f6d7
Katerina Solstad (katya sorok) <37941283+katerinaSo@users.noreply.github.com>
2020-10-01 21:37:35 -0400

    Add files via upload

---
+++ Tower_of_Hanoi.py
@@ -0,0 +1,24 @@
+# Tower of Hanoi (homework)
+"""
+   The puzzle was invented by the French mathematician Édouard Lucas in 1883.
+   Numerous myths regarding the ancient and mystical nature of the puzzle
+   popped up almost immediately.
+   There is a story about an Indian temple in Kashi Vishwanath which contains
+   a large room with three time-worn posts in it, surrounded by 64 golden disks.
+   Brahmin priests, acting out the command of an ancient prophecy, have been moving
+   these disks in accordance with the immutable rules of Brahma since that time.
+   The puzzle is therefore also known as the Tower of Brahma puzzle.
+   According to the legend, when the last move of the puzzle is completed, the world will end.
+   
+   If the legend were true, and if the priests were able to move disks at a rate of one per second,
+   using the smallest number of moves it would take them (2**64 − 1) seconds ( 2 in power of 64 minus 1)
+   
+   *****************************************************************************************************
+   Your program should give the output how many years and days it is.
+   Is the end of the world near?????
+   
+"""
+
+# help:
+# math operations: **, *, //, %, -, +
+# functions: print(), int(), str()
